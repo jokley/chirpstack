27dbbd2d0acf30a63722f74380ca83b19800b06b
jokley <reinhard_juen@vol.at>
2023-03-21 14:26:22 +0100

    Update app.py

--- configuration/backend/app.py
+++ configuration/backend/app.py
@@ -127,9 +127,9 @@ def time():
 def influx():
 
     data = get_min_max_values()
-    tempMin = data['temperatureMin']
+    #tempMin = data['temperatureMin']
     
-    return jsonify(tempMin)
+    return (data)
 
 @app.route('/venti',methods = ['POST', 'GET'])
 def switch():
