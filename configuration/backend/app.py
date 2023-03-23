cf2c975118bf8a6b0edfcdd016a9280da782889d
jokley <reinhard_juen@vol.at>
2023-03-23 15:42:24 +0100

    Update app.py

--- configuration/backend/app.py
+++ configuration/backend/app.py
@@ -64,7 +64,8 @@ def venti_control():
     startTime = dataVenti[0]['mode'][0]
     mode = dataVenti[0]['mode'][1]
     tsSoll =dataVenti[0]['trockenMasseSoll'][1]
-    stock = dataVenti[0]['stockaufbau'][1]
+    stock = int(dataVenti[0]['stockaufbau'][1])
+    stock *= 3600
 
     dataLastTime = get_venti_lastTimeOn()
     lastOn = dataLastTime[0]['lastTimeOn']
@@ -296,7 +297,8 @@ def influx():
     startTime = dataVenti[0]['mode'][0]
     mode = dataVenti[0]['mode'][1]
     tsSoll =dataVenti[0]['trockenMasseSoll'][1]
-    stock = dataVenti[0]['stockaufbau'][1]
+    stock = int(dataVenti[0]['stockaufbau'][1])
+    stock *= 3600
 
     dataLastTime = get_venti_lastTimeOn()
     lastOn = dataLastTime[0]['lastTimeOn']
@@ -312,7 +314,7 @@ def influx():
    
 
 
-    return jsonify('{},{},{}'.format((stock*3600),remainingTimeStock,remainingTimeInterval))
+    return jsonify('{},{},{}'.format(stock,remainingTimeStock,remainingTimeInterval))
     #return jsonify(dataVenti[0]['mode'][0])
     #return jsonify('{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(humMin, humMax,tempMin,tempMax,tsMin,tsMax,humOut,tempOut,tsOut,startTimeStock,mode,tsSoll,stock))
 
