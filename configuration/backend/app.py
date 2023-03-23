01f1f7e3f9c05d196403109367a6e404492585a8
jokley <reinhard_juen@vol.at>
2023-03-23 12:49:40 +0100

    Update app.py

--- configuration/backend/app.py
+++ configuration/backend/app.py
@@ -228,7 +228,7 @@ def influx():
     DST =  get_timestamp_now_offset()
     #startTimeStock = datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S.%f') + datetime.timedelta(seconds=DST)
     # startTimeStock = int(datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S.%f').date())
-    startTimeStock = datetime(startTime).replace(tzinfo=timezone.utc).timestamp() + timedelta(seconds=DST)
+    startTimeStock = startTime.replace(tzinfo=timezone.utc).timestamp() + timedelta(seconds=DST)
     timeNow = get_timestamp_now_epoche()
     #remainingTimeStock =     timeNow - startTimeStock
     remainingTimeStock = 0
