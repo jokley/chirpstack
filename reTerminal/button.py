ab0f4b561338231fe5472d4e038c8f1e8c97f25e
jokley <reinhard_juen@vol.at>
2023-05-16 22:01:28 +0200

    buttons

--- reTerminal/button.py
+++ reTerminal/button.py
@@ -15,18 +15,29 @@ while True:
     for event in device.read_loop():
         buttonEvent = rt_btn.ButtonEvent(event)
         if str(buttonEvent.name) == "ButtonName.F1" and buttonEvent.value == 1:
-            requests.post('http://172.16.238.19/backend/venti', json={'cmd':'on','tm':85,'stock':'0'})
+            requests.post('http://172.16.238.19/backend/venti', json={'cmd':'on','tm':87,'stock':'0'})
             pyautogui.hotkey('f5')
             print(f"Button Ein")
         if str(buttonEvent.name) == "ButtonName.F2" and buttonEvent.value == 1:
-            requests.post('http://172.16.238.19/backend/venti', json={'cmd':'off','tm':81,'stock':'0'})
+            requests.post('http://172.16.238.19/backend/venti', json={'cmd':'off','tm':87,'stock':'0'})
             pyautogui.hotkey('f5')
             print(f"Button Aus")
         if str(buttonEvent.name) == "ButtonName.F3" and buttonEvent.value == 1:
             requests.post('http://172.16.238.19/backend/venti', json={'cmd':'auto','tm':87,'stock':'0'})
             pyautogui.hotkey('f5')
             print(f"Button Auto")
-        if str(buttonEvent.name) == "ButtonName.O" and buttonEvent.value == 1:
-            time.sleep(3)
-            os.system("sudo shutdown now -h")
-            print(f"Button 0")
+        if str(buttonEvent.name) == "ButtonName.O":
+            pyautogui.hotkey('esc')
+            off = time.time()
+            if buttonEvent.value == 1:
+                on = time.time()
+            if buttonEvent.value == 0:
+                off = time.time()
+            duration = off-on
+            if duration >0 and duration <= 1:
+                pyautogui.hotkey('esc')
+                pyautogui.hotkey('f5')
+            if duration >= 2:
+                 pyautogui.hotkey('esc')
+                 print(f"Schutdown")
+                 os.system("sudo shutdown now -h")
