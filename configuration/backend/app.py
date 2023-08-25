e5676ca70c2b1ffcca1a1311cd379131fd966761
jokley <reinhard_juen@vol.at>
2023-08-25 09:22:21 +0200

    Update app.py

--- configuration/backend/app.py
+++ configuration/backend/app.py
@@ -490,7 +490,7 @@ def controlValues():
 def controlParamValues():
    
     pramsVenti = get_venti_control_param_values()
-    startTime = pramsVenti[0]['sdef_on'][0]
+    #startTime = pramsVenti[0]['sdef_on'][0]
     sdef_on = pramsVenti[0]['sdef_on'][1]/10
     sdef_hys = pramsVenti[0]['sdef_hys'][1]/10
     uschutz_on = pramsVenti[0]['uschutz_on'][1]/10
