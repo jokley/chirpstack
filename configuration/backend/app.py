271e4f826503280bcd4e1c6ee05abd3057755e58
jokley <reinhard_juen@vol.at>
2024-05-03 13:37:00 +0200

    last off

--- configuration/backend/app.py
+++ configuration/backend/app.py
@@ -106,7 +106,7 @@ def venti_control():
     lastTimeOn = (lastOn + timedelta(seconds=DST)).replace(tzinfo=timezone.utc).timestamp() 
     lastTimeOff = (lastOff + timedelta(seconds=DST)).replace(tzinfo=timezone.utc).timestamp() 
     remainingTimeStock =     int(timeNow - startTimeStock)
-    remainingTimeInterval =  int(lastTimeOff - timeNow)
+    remainingTimeInterval =  int(lastTimeOff - lastTimeOn)
 
     pramsVenti = get_venti_control_param_values()
     #startTime = pramsVenti[0]['sdef_on'][0]
