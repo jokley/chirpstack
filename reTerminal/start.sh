9112583cfd6b00a719dac77b150a6ea72ac3cd62
jokley <reinhard_juen@vol.at>
2024-05-17 08:33:29 +0200

    noerrdialogs

--- reTerminal/start.sh
+++ reTerminal/start.sh
@@ -6,5 +6,5 @@ while ! curl -s --head --request GET http://172.16.238.19/ | grep "200 OK" > /de
     sleep 1
 done
 
-# Once the website is accessible, launch the browser in fullscreen mode
-chromium-browser --start-fullscreen http://172.16.238.19/ --display=:0 &
+# Once the website is accessible, launch the browser in fullscreen mode without error dialogs
+chromium-browser --start-fullscreen --noerrdialogs http://172.16.238.19/ --display=:0 &
