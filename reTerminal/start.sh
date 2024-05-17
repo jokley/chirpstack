d27f46358307ad62c4d71b9d7ed7e431232f35e3
jokley <reinhard_juen@vol.at>
2024-05-17 07:48:26 +0200

    change sh script

--- reTerminal/start.sh
+++ reTerminal/start.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 export DISPLAY=:0
-sleep 15;
-chromium-browser --start-fullscreen http://172.16.238.19/  --display=:0 &
+
+# Wait until Grafana website is accessible
+while ! curl -s --head --request GET http://172.16.238.19/ | grep "200 OK" > /dev/null; do
+    sleep 1
+done
+
+# Once the website is accessible, launch the browser in fullscreen mode
+chromium-browser --start-fullscreen http://172.16.238.19/ --display=:0 &
