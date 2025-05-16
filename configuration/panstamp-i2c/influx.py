29a6724946757932565eca674912318fe1a6b0b4
jokley <reinhard_juen@vol.at>
2025-05-16 22:35:00 +0200

    Update influx.py

--- configuration/panstamp-i2c/influx.py
+++ configuration/panstamp-i2c/influx.py
@@ -3,7 +3,7 @@ import logging
 from influxdb_client import InfluxDBClient, WriteOptions
 
 # Configure logging
-LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
+LOG_LEVEL = "INFO"
 logging.basicConfig(level=LOG_LEVEL,
                     format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
@@ -16,7 +16,7 @@ def get_influxdb_client():
     INFLUXDB_URL = "http://172.16.238.16:8086"
     INFLUXDB_TOKEN = os.getenv("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN")
     INFLUXDB_ORG = os.getenv("DOCKER_INFLUXDB_INIT_ORG")
-    logger.debug(f"Connecting to InfluxDB at {INFLUXDB_URL}, org={INFLUXDB_ORG}")
+    logger.(f"Connecting to InfluxDB at {INFLUXDB_URL}, org={INFLUXDB_ORG}")
     return InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 
 def write_to_influx(line_protocol: str):
@@ -28,7 +28,7 @@ def write_to_influx(line_protocol: str):
     write_api = client.write_api(write_options=WriteOptions(batch_size=1))
     
     try:
-        logger.debug(f"InfluxDB line protocol to write:\n{line_protocol}")
+        logger.(f"InfluxDB line protocol to write:\n{line_protocol}")
         write_api.write(bucket=BUCKET, org=client.org, record=line_protocol)
         logger.info("✅ Data written to InfluxDB")
     except Exception as e:
