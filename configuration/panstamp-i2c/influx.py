fdcd997792e1b1aa3a09b5959ff457b9366050e1
jokley <reinhard_juen@vol.at>
2025-05-16 10:14:15 +0200

    Create influx.py

---
+++ configuration/panstamp-i2c/influx.py
@@ -0,0 +1,23 @@
+import os
+from influxdb_client import InfluxDBClient, WriteOptions
+
+def get_influxdb_client():
+    INFLUXDB_URL = os.getenv("INFLUXDB_URL")
+    INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
+    INFLUXDB_ORG = os.getenv("INFLUXDB_ORG")
+
+    return InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+
+def write_to_influx(line_protocol: str):
+    BUCKET = os.getenv("DOCKER_INFLUXDB_INIT_BUCKET")
+    client = get_influxdb_client()
+    write_api = client.write_api(write_options=WriteOptions(batch_size=1))
+    
+    try:
+        write_api.write(bucket=BUCKET, org=client.org, record=line_protocol)
+        print("✅ Data written to InfluxDB")
+    except Exception as e:
+        print(f"❌ Error writing to InfluxDB: {e}")
+    finally:
+        write_api.__del__()
+        client.__del__()
