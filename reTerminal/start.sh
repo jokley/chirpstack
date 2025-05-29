824410fdd4c9529e01cc2966c4bc6e3ba9aa27b4
jokley <reinhard_juen@vol.at>
2025-05-29 18:24:10 +0200

    Update start.sh

--- reTerminal/start.sh
+++ reTerminal/start.sh
@@ -7,4 +7,4 @@ while ! curl -s --head --request GET http://172.16.238.19/ | grep "200 OK" > /de
 done
 
 # Once the website is accessible, launch the browser in fullscreen mode without error dialogs
-chromium-browser --start-fullscreen --noerrdialogs http://172.16.238.19/ --display=:0 &
+chromium-browser --start-fullscreen --noerrdialogs http://172.16.238.19?kiosk=1 --display=:0 &
