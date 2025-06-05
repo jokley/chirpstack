326a896dbd011d96698ddfe9c7acde533fe118bd
jokley <reinhard_juen@vol.at>
2025-06-05 09:53:37 +0200

    Update main.py

--- configuration/panstamp-i2c/main.py
+++ configuration/panstamp-i2c/main.py
@@ -13,7 +13,7 @@ from influx import write_to_influx
 from mqtt_handler import setup_mqtt
 
 # Configure logging
-LOG_LEVEL = "INFO"
+LOG_LEVEL = "ERROR"
 logging.basicConfig(level=LOG_LEVEL,
                     format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
